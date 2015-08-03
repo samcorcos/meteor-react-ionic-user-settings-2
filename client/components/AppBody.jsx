10b02ec782ea8b92adc027e3237bdafe00cc7aa6
Sam Corcos <sam@corcos.io>
2015-08-02 17:21:48 -0700

    completed from article

--- client/components/AppBody.jsx
+++ client/components/AppBody.jsx
@@ -1,4 +1,23 @@
+let Transition = React.addons.CSSTransitionGroup;
+
 AppBody = React.createClass({
+  getInitialState() {
+    return {
+      modal: false
+    }
+  },
+  ionModal(tab) {
+    this.setState({
+      modal: (
+        <IonModal>
+          <div className="h1 title">{tab}</div>
+          <button onClick={ () => this.setState({modal:false}) } className="button button-icon active">
+            <i className="icon ion-ios-close-empty"></i>
+          </button>
+        </IonModal>
+      )
+    })
+  },
   render() {
     return (
       <div className="ionic-body">
@@ -8,10 +27,15 @@ AppBody = React.createClass({
           <ReactRouter.Link className="button button-icon icon ion-heart" to={"/other"}></ReactRouter.Link>
         </div>
 
+        {this.state.modal ? <Backdrop /> : false}
+        <Transition transitionName="modal">
+          {this.state.modal}
+        </Transition>
+
         <div className="view">
           <div className="scroll-content ionic-scroll">
             <div className="content overflow-scroll has-header">
-              <ReactRouter.RouteHandler />
+              <ReactRouter.RouteHandler ionModal={this.ionModal}/>
             </div>
           </div>
         </div>
--- client/components/Settings.jsx
+++ client/components/Settings.jsx
@@ -2,8 +2,8 @@ Settings = React.createClass({
   render() {
     return (
       <div>
-        <Profile />
-        <SettingsList />
+        <Profile ionModal={this.props.ionModal} />
+        <SettingsList ionModal={this.props.ionModal} />
       </div>
     )
   }
@@ -12,9 +12,19 @@ Settings = React.createClass({
 Profile = React.createClass({
   mixins: [ReactMeteorData],
   getMeteorData() {
+    if (Meteor.loggingIn() || !Meteor.user()) {this.setState({loginStatus: false})};
+    if (Meteor.user()) {this.setState({loginStatus: true})};
     return {
       user: Meteor.user(),
       userLoading: Meteor.loggingIn()
+    }
+  },
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.loginStatus !== this.state.loginStatus;
+  },
+  getInitialState() {
+    return {
+      loginStatus: false
     }
   },
   render() {
@@ -30,10 +40,32 @@ Profile = React.createClass({
           <img src={this.data.user.profile.image} />
         </div>
         <div className="login-wrapper">
-          Logout
+          {this.state.loginStatus ? <LoggedIn ionModal={this.props.ionModal} /> : <NotLoggedIn ionModal={this.props.ionModal} />}
         </div>
       </div>
     )
+  }
+})
+
+LoggedIn = React.createClass({
+  logout() {
+    Meteor.logout();
+  },
+  render() {
+    return (
+      <div>
+        <a onClick={this.logout}>Logout</a>
+      </div>
+    )
+  }
+})
+
+NotLoggedIn = React.createClass({
+  login(user, pass) {
+    Meteor.loginWithPassword(user, pass);
+  },
+  render() {
+    return <a>Login</a>
   }
 })
 
@@ -46,7 +78,7 @@ SettingsList = React.createClass({
   render() {
     let list = this.props.settings.map((setting) => {
       return (
-        <div className="item" key={setting}>
+        <div onClick={this.props.ionModal.bind(null, setting)} className="item" key={setting}>
           <h2><a>{setting}</a></h2>
         </div>
       )
